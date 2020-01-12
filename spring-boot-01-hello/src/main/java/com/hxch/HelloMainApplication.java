996466c38e50b5a11b1e483bd3fb4565f455277e
PangZiShenTiHao <1286287975@qq.com>
2020-01-12 19:57:12 +0800

    项目初始化 访问首页

---
+++ spring-boot-01-hello/src/main/java/com/hxch/HelloMainApplication.java
@@ -0,0 +1,15 @@
+package com.hxch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动
+ */
+@SpringBootApplication
+public class HelloMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMainApplication.class, args);
+
+    }
+}
