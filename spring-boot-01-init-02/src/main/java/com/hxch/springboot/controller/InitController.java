a866da7f4cd21eeb8adc06050ed4de478812e096
huangxc <huangxianc@jkinvest.com>
2020-01-15 11:41:21 +0800

    init 启动

--- spring-boot-01-init-02/src/main/java/com/hxch/springboot/controller/InitController.java
+++ spring-boot-01-init-02/src/main/java/com/hxch/springboot/controller/InitController.java
@@ -1,4 +1,4 @@
-package com.hxch.controller;
+package com.hxch.springboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
