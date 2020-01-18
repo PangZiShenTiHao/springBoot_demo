d97069086485c6da3afafb6d1d3edafbe093a4f7
PangZiShenTiHao <1286287975@qq.com>
2020-01-18 19:55:04 +0800

    web项目bill初始化

---
+++ spring-boot-06-bill/src/main/java/com/hxch/springboot/SpringBoot06BillApplication.java
@@ -0,0 +1,13 @@
+package com.hxch.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot06BillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot06BillApplication.class, args);
+    }
+
+}
---
+++ spring-boot-06-bill/src/main/java/com/hxch/springboot/controller/ProviderController.java
@@ -0,0 +1,13 @@
+package com.hxch.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProviderController {
+
+    @GetMapping("/providers")
+    public String list(){
+        return "provider/list";
+    }
+}
