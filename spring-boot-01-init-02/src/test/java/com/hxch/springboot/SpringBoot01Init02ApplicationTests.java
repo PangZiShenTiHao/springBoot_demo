a12a33a959a232394b7f7c873e4c772843fe604f
huangxc <huangxianc@jkinvest.com>
2020-01-22 10:51:28 +0800

    加入首页

--- spring-boot-01-init-02/src/test/java/com/hxch/springboot/SpringBoot01Init02ApplicationTests.java
+++ spring-boot-01-init-02/src/test/java/com/hxch/springboot/SpringBoot01Init02ApplicationTests.java
@@ -5,6 +5,8 @@ import com.hxch.springboot.service.EmpService;
 import com.hxch.springboot.service.EmpService02;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +15,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBoot01Init02ApplicationTests {
+
+    //日志记录器
+    Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     private Emp emp;
@@ -38,6 +43,17 @@ public class SpringBoot01Init02ApplicationTests {
 
         EmpService02 empService02 = (EmpService02) context.getBean("empService02");
         empService02.add2();
+    }
+
+    @Test
+    public void testLog(){
+        logger.trace(" 这是 trace 日志信息！");
+        logger.debug(" 这是 debug 日志信息！");
+        logger.info(" 这是 info 日志信息！");
+        logger.warn(" 这是 warn 日志信息！");
+        logger.error(" 这是 error 日志信息！");
+
+
     }
 
 }
